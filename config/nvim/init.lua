6bf3e5deea9c44ab16c062277bcc07c6ff8eee30
Ivan Fernandez Villar <ivanfern4ndez@gmail.com>
2026-01-12 03:09:24 +0100

    config:nvim: Fix neovim day zero bootstrap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -10,16 +10,45 @@ if not NVIM_PLUG_INSTALLED then
         "--create-dirs",
         NVIM_PLUG_URL,
     })
+    vim.cmd("source " .. NVIM_PLUG_DESTINATION)
     vim.cmd("autocmd VimEnter * PlugInstall --sync | source $MYVIMRC")
 end
 
 require("core.options")
 require("core.autocmd")
 require("core.filetypes")
+require("plugins")
 
 if NVIM_PLUG_INSTALLED then
-    require("plugins")
     require("core.keymaps")
     require("core.settings")
     require("core.highlights")
+
+    -- Color Scheme
+    local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME") or "catppuccin-mocha"
+    vim.cmd("colorscheme " .. NVIM_COLORSCHEME)
+
+    --- Import Plugins config
+    require("plugins.avante")
+    require("plugins.cmp")
+    require("plugins.dap")
+    require("plugins.mason")
+    require("plugins.linters")
+    require("plugins.formatter")
+    require("plugins.dev")
+    require("plugins.lsp")
+    require("plugins.mini")
+    require("plugins.misc")
+    require("plugins.neogen")
+    require("plugins.neogit")
+    require("plugins.neotree")
+    require("plugins.noice")
+    require("plugins.snacks")
+    require("plugins.statusline")
+    require("plugins.tabby")
+    require("plugins.telescope")
+    require("plugins.terminal")
+    require("plugins.treesitter")
+    require("plugins.windows")
+    require("plugins.diagnostic")
 end
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -129,31 +129,3 @@ Plug("manoelcampos/xml2lua")
 Plug("lunarmodules/lua-mimetypes")
 
 vim.call("plug#end")
-
--- Color Scheme
-local NVIM_COLORSCHEME = os.getenv("NVIM_COLORSCHEME") or "catppuccin-mocha"
-vim.cmd("colorscheme " .. NVIM_COLORSCHEME)
-
---- Import Plugins config
-require("plugins.avante")
-require("plugins.cmp")
-require("plugins.dap")
-require("plugins.mason")
-require("plugins.linters")
-require("plugins.formatter")
-require("plugins.dev")
-require("plugins.lsp")
-require("plugins.mini")
-require("plugins.misc")
-require("plugins.neogen")
-require("plugins.neogit")
-require("plugins.neotree")
-require("plugins.noice")
-require("plugins.snacks")
-require("plugins.statusline")
-require("plugins.tabby")
-require("plugins.telescope")
-require("plugins.terminal")
-require("plugins.treesitter")
-require("plugins.windows")
-require("plugins.diagnostic")
