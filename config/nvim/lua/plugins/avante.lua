940c0a1abde2b5cd9f080f29c92ac999fed5b127
Ivan Fernandez Villar <ivanfern4ndez@gmail.com>
2026-01-17 01:45:50 +0100

    config:nvim: update avante config

--- config/nvim/lua/plugins/avante.lua
+++ config/nvim/lua/plugins/avante.lua
@@ -1,23 +1,36 @@
 require("avante").setup({
+    mode = "agentic",
     provider = "gemini",
+    instructions_file = "AGENT.md",
     providers = {
         claude = {
             endpoint = "https://api.anthropic.com",
-            model = "claude-sonnet-4-20250514",
+            model = "claude-sonnet-4-5-20250929",
             disable_tools = true,
             timeout = 30000,
             extra_request_body = {
-                temperature = 0,
+                temperature = 0.65,
                 max_tokens = 8192,
             },
         },
         gemini = {
             endpoint = "https://generativelanguage.googleapis.com/v1beta/models",
-            model = "gemini-2.5-flash-preview-09-2025",
+            model = "gemini-2.5-flash",
             timeout = 30000,
+            context_window = 32768,
             extra_request_body = {
-                temperature = 0.25,
+                generationConfig = {
+                    temperature = 0.65,
+                },
             },
+        },
+    },
+    input = {
+        provider = "snacks",
+        provider_opts = {
+            title = "Avante Input",
+            icon = "󰯄 ",
+            placeholder = "Enter your API key...",
         },
     },
     behaviour = {
